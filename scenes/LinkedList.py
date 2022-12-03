b716b149b9e38ef830ec192392e2d488ae6d948a
Shamik Patro <patroshamik@gmail.com>
2022-12-03 17:59:59 -0600

    Added Von Neumann Video

--- scenes/LinkedList.py
+++ scenes/LinkedList.py
@@ -1,11 +1,5 @@
 from manim import *
 
-class OpeningText(Scene):
-    def construct(self):
-        title = Tex(r"Linked Lists")
-        self.play(Write(title))
-        self.wait(3)
-
 class LinkedList(Scene):
     def construct(self):
         title = Tex(r"Another Scene")
